445548aa100e9ba62cc307a8b3a1d6307bd56e9f
Tuchentia <47703068+Tuchentia@users.noreply.github.com>
2020-03-07 01:06:57 +0300

    Update Bot.java

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -6,7 +6,11 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
 import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Bot extends TelegramLongPollingBot {
     private enum Command {
@@ -42,9 +46,34 @@ public class Bot extends TelegramLongPollingBot {
     
     //сейчас возвращаемое значение не используется, если что, сделаю void
     private Date correctDateFormat(String date) throws ParseException {
-        return (new SimpleDateFormat("dd/MM/yyyy").parse(date));
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        format.setLenient(false);
+        return format.parse(date);
     }
-
+    
+    private void add(Long chatId, String text) throws BotException {
+        try {
+            FileWriter writer = new FileWriter(new File("output.txt"), true);
+            PrintWriter printWriter = new PrintWriter(writer);
+            String[] tokens = text.trim().split(" ");
+            if (tokens.length < 2) {
+                throw new BotException("Invalid event format.\nFormat should be: DD.MM.YYYY your event\nTry again");
+            }
+            try {
+                correctDateFormat(tokens[0].replace('.', '/'));
+            } catch (ParseException e) {
+                throw new BotException("Invalid date format.\nFormat should be: DD.MM.YYYY\nTry again");
+            }
+            printWriter.println(chatId + " " + text);
+            printWriter.close();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new BotException("Unable to enter event now.\nPlease try again later");
+        }
+    }
+   
+    /*
     private boolean add(Long chatId, String text) {
         try {
             FileWriter writer = new FileWriter(new File("output.txt"), true);
@@ -68,6 +97,7 @@ public class Bot extends TelegramLongPollingBot {
         }
         return true;
     }
+    */
 
     /*
     private  void check(String text) throws Exception {
@@ -85,7 +115,30 @@ public class Bot extends TelegramLongPollingBot {
         if (tokens.length < 2 || tokens[1].length() == 0) throw new Exception("enter event");
     }
     */
-
+    
+    private void show(Message message) throws BotException {
+        //  it will show all the events
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("output.txt")))) {
+            String s = "";
+            while (s != null) {
+                if (s.length() >= 2) {
+                    String[] strings = s.split(" ");
+                    System.out.println(strings[0] + " " + strings[1]);
+                    if (Long.parseLong(strings[0]) == message.getChatId()) {
+                        stringBuilder.append(s.substring(strings[0].length())).append("\n");
+                    }
+                }
+                s = reader.readLine();
+            }
+            sendMsg(message, stringBuilder.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new BotException("Unable to show events now.\nPlease try again later");
+        }
+    }
+    
+    /*
     private void show(Message message) {
         //  it will show all the events
         StringBuilder stringBuilder = new StringBuilder();
@@ -118,7 +171,68 @@ public class Bot extends TelegramLongPollingBot {
             sendMsg(message, "error");
         }
     }
+    */
+    
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        try {
+            if (message != null && message.hasText()) {
+                String messageText = message.getText();
+                switch (messageText) {
+                    case "/start":
+                        sendMsg(message, "I'm the Calendar Bot! I can do stuff!\n" +
+                                "Try one of my commands: \n/help\n/add\n/show\n/delete");
+                        currentCommand = Command.NONE;
+                        break;
+                    case "/add":
+                        currentCommand = Command.ADD;
+                        sendMsg(message, "Send me the date and the event like this: DD.MM.YYYY your event");
+                        break;
+                    case "/help":
+                        sendMsg(message, "I'm ready to help!");
+                        currentCommand = Command.NONE;
+                        break;
+                    case "/show":
+                        show(message);
+                        currentCommand = Command.NONE;
+                        break;
+            /*
+            case "/delete":
+                currentCommand = Command.DELETE;
+                show(message);
+                sendMsg(message, "Please send me the number of the event you want to delete");
+            */
+                    case "/stop":
+                        currentCommand = Command.NONE;
+                        sendMsg(message, "Ready for the new command!");
+                        break;
+                    default:
+                        if (currentCommand.equals(Command.NONE)) {
+                            sendMsg(message, "I don't know what you mean :(\n" +
+                                    "Try one of these: \n/help\n/add\n/show\n/delete");
+                        } else {
+                            switch (currentCommand) {
+                                case ADD:
+                                    add(message.getChatId(), messageText);
+                                    sendMsg(message, "Added successfully!");
+                                    currentCommand = Command.NONE;
+                                    break;
+                                case DELETE:
+                                    // TODO: delete the event
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        break;
+                }
+            }
+        } catch (BotException e) {
+            sendMsg(message, e.getMessage());
+        }
+    }
 
+    /*
     public void onUpdateReceived(Update update) {
         Message message = update.getMessage();
         if (message != null && message.hasText()) {
@@ -140,13 +254,13 @@ public class Bot extends TelegramLongPollingBot {
                 show(message);
                 currentCommand = Command.NONE;
             }
-            /*
-            else if (messageText.equals("/delete")) {
-                currentCommand = Command.DELETE;
-                show(message);
-                sendMsg(message, "Please send me the number of the event you want to delete");
-            }
-            */
+           
+//             else if (messageText.equals("/delete")) {
+//                 currentCommand = Command.DELETE;
+//                 show(message);
+//                 sendMsg(message, "Please send me the number of the event you want to delete");
+//             }
+            
             else if (messageText.equals("/stop")) {
                 currentCommand = Command.NONE;
                 sendMsg(message, "Ready for the new command!");
@@ -176,6 +290,7 @@ public class Bot extends TelegramLongPollingBot {
             }
         }
     }
+    */
 
     public String getBotUsername() {
         return "TheFriendliestDoveBot";
